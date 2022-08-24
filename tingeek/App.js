e1351f4fa281396b5e75b9c67bef0ee0d11049ba
CheeseMold <henriquebear17@gmail.com>
2022-08-24 16:33:25 -0300

    Adicionando o movimento das "cartas".

--- tingeek/App.js
+++ tingeek/App.js
@@ -1,36 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Animated, Image } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Animated, Image, PanResponder } from 'react-native';
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
-const SCREEN_WIDTH = Dimensions.get('window').widthw;
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 // Array com imagens
-const Profiles = [
-	{ id: "1", name: "Better Call Saul", uri: require("./assets/better-call-saul.jpg") },
-	{ id: "2", name: "Breaking Bad", uri: require("./assets/breaking-bad.jpg") },
-	{ id: "3", name: "Cuphead Show", uri: require("./assets/cuphead.jpeg") },
-	{ id: "4", name: "Stranger Things", uri: require("./assets/stranger_things.jpg") },
-	{ id: "5", name: "The Walking Dead", uri: require("./assets/w-d.jpg") }
+const Profile = [
+	{id: "1", name: "Skyrim", uri: require('./assets/1.jpg')},
+	{id: "2", name: "Dark Souls", uri: require('./assets/2.jpg')},
+	{id: "3", name: "Metroid Prime", uri: require('./assets/3.jpg')},
+	{id: "4", name: "Zelda Twilight Princess", uri: require('./assets/5.jpg')},
+	{id: "5", name: "Zelda A Link Between Worlds", uri: require('./assets/6.jpg')},
+	{id: "6", name: "Mario 3D Land", uri: require('./assets/7.jpg')},
+	{id: "7", name: "Castlevania Sotn", uri: require('./assets/8.jpg')},
+	{id: "8", name: "Subnautica Below Zero", uri: require('./assets/9.png')}
+
+	// { id: "1", name: "Better Call Saul", uri: require("./assets/better-call-saul.jpg") },
+	// { id: "2", name: "Breaking Bad", uri: require("./assets/breaking-bad.jpg") },
+	// { id: "3", name: "Cuphead Show", uri: require("./assets/cuphead.jpeg") },
+	// { id: "4", name: "Stranger Things", uri: require("./assets/stranger_things.jpg") },
+	// { id: "5", name: "The Walking Dead", uri: require("./assets/w-d.jpg") }
 ];
 
 
 
 export default class App extends React.Component {
-	render() {
-		return (
-			// Estrutura basica do app
-			<View style={{flex: 1}}>
-				<View style={{height: 60}}></View>
-				<View style={{flex: 1}}>
-					<Animated.View style={[
-							{
-								height: SCREEN_HEIGHT - 120, // - 120 para ter a altura entre o cabecalho e rodape (60 + 60)
-								width: SCREEN_WIDTH, // Cabecalho e rodape nao irao impactar na largura
-								padding: 20
-							}
-						]}>
-						
+
+	constructor() {
+		super();
+		this.position = new Animated.ValueXY();
+		this.state = {
+			currentIndex: 0
+		}
+	}
+	
+	componentWillMount() {
+		this.PanResponder = PanResponder.create({
+			onStartShouldSetPanResponder: (evt, gestureState) => true,
+			onPanResponderMove: (evt, gestureState) => {
+				this.position.setValue({x: gestureState.dx, y: gestureState.dy});
+			},
+			onPanResponderRelease: (evt, gestureState) => {
+
+			}
+		});
+	};
+
+	renderProfile = () => {
+		return Profile.map((item, i) => {
+			
+			if (this.state.currentIndex > i) return null;
+			else if (this.state.currentIndex == i) { 
+				return (
+					<Animated.View 
+						{...this.PanResponder.panHandlers}
+						key={item.id}
+						style={[
+						{
+							transform: this.position.getTranslateTransform()
+						},
+						{
+							height: SCREEN_HEIGHT - 120, // - 120 para ter a altura entre o cabecalho e rodape (60 + 60)
+							width: SCREEN_WIDTH, // Cabecalho e rodape nao irao impactar na largura
+							padding: 10, 
+							position: 'absolute'
+						}
+					]}>
+					
 						<Image 
 						style={{ // Primeira imagem
 							flex: 1,
@@ -39,8 +76,46 @@ export default class App extends React.Component {
 							resizeMode: "cover",
 							borderRadius: 20
 						}} 
-						source={Profiles[0].uri}/>
+						source={item.uri}/>
 					</Animated.View>
+				);
+			}
+			
+			else {
+				return (
+					<Animated.View 
+						key={item.id}
+						style={[
+						{
+							height: SCREEN_HEIGHT - 120, // - 120 para ter a altura entre o cabecalho e rodape (60 + 60)
+							width: SCREEN_WIDTH, // Cabecalho e rodape nao irao impactar na largura
+							padding: 10, 
+							position: 'absolute'
+						}
+					]}>
+					
+						<Image 
+						style={{ // Primeira imagem
+							flex: 1,
+							height: null,
+							width: null,
+							resizeMode: "cover",
+							borderRadius: 20
+						}} 
+						source={item.uri}/>
+					</Animated.View>
+				);
+			}
+		}).reverse();
+	};
+
+	render() {
+		return (
+			// Estrutura basica do app
+			<View style={{flex: 1}}>
+				<View style={{height: 60}}></View>
+				<View style={{flex: 1}}>
+					{this.renderProfile()}
 				</View>
 				<View style={{height: 60}}></View>
 			</View>
